bc76c72d79c8275931605db3cbbeac1e6123e9c9
Martin Schmidt <martin.schmidt@epita.fr>
2019-03-04 12:12:25 +0000

    Added LD (BC), A

--- src/decode.rs
+++ src/decode.rs
@@ -16,6 +16,9 @@ pub fn decode_instruction(opcode: u8, mmu: &MMU, regs: &Registers) -> String {
             let d8 = mmu.read(regs.pc + 1);
             String::from(format!("LD C, {:2X}", d8))
         },
+        0x20 => {
+            String::from(format!("LD (BC), A (BC = {:4X}, A = {:2X})", regs.get_bc(), regs.a))
+        },
         0x21 => {
             let d16 = mmu.read_wide(regs.pc + 1);
             String::from(format!("LD HL, {:4X}", d16))
